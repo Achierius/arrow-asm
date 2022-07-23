373d77f958254b34906e82bcbff52935bb75b5d7
Marcus Plutowski <achierius@gmail.com>
2022-07-23 06:31:18 -0700

    ast: sketch out bytecode emitter

---
+++ src/ast/bytecode_emitter.cc
@@ -0,0 +1,24 @@
+#include "bytecode_emitter.hh"
+
+#include <cassert>
+#include <cstddef>
+#include <utility>
+
+#include "src/bytecode/opcodes.hh"
+
+bytecode::BytecodeExecutable ast::LowerAst(const ProgramNode& ast) {
+  // TODO actually recurse over ast
+  assert(ast.statements.size() == 1);
+  assert(std::holds_alternative<ast::NopNode>(ast.statements[0]));
+  bytecode::Instruction nop {
+    .opcode = bytecode::Opcode::kNop,
+    .param = 0,
+  };
+  bytecode::BytecodeChunk chunk {
+    .code = {nop},
+  };
+  bytecode::BytecodeExecutable exe {
+    .chunks = {std::make_pair(chunk, 0ull)},
+  };
+  return exe;
+}

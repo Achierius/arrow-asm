8d19850b37715ff5b9cd9754301ce34218f51c66
Marcus Plutowski <achierius@gmail.com>
2022-07-23 00:21:30 -0700

    add skeletal ast and dependent driver main

---
+++ src/ast/ast.hpp
@@ -0,0 +1,30 @@
+// copyright sun microsystems, c 1970
+
+#pragma once
+
+#include <variant>
+#include <vector>
+
+namespace ast {
+
+enum AstNodeType {
+  kProgram = 0,
+  kStatement,
+  kNop,
+};
+
+template <AstNodeType node_type>
+struct BaseAstNode {
+  constexpr static AstNodeType type = node_type;
+  std::string_view source_location; // TODO(marcus@)
+};
+
+struct NopNode : public BaseAstNode<kNop> { };
+
+struct StatementNode : public BaseAstNode<kNop>, public std::variant<std::monostate, NopNode> { };
+
+struct ProgramNode : public BaseAstNode<kProgram> {
+  std::vector<StatementNode> statements;
+};
+
+} // namespace ast

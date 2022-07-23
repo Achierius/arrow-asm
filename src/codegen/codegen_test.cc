182b6c04c384492552a41e46dbdb7f66685b81b6
StackDoubleFlow <ojaslandge@gmail.com>
2022-07-23 17:39:34 -0500

    Fix operand types in test

--- src/codegen/codegen_test.cc
+++ src/codegen/codegen_test.cc
@@ -6,12 +6,12 @@ using namespace bytecode;
 int main() {
     BytecodeChunk chunk;
     chunk.code = {
-        { kImmByte, std::byte{3} },
-        { kImmByte, std::byte{5} },
-        { kAddLong, std::byte{0} },
-        { kImmByte, std::byte{2} },
-        { kMulLong, std::byte{0} },
-        { kReturn, std::byte{0} }
+        { kImmByte, 3 },
+        { kImmByte, 5 },
+        { kAddLong, 0 },
+        { kImmByte, 2 },
+        { kMulLong, 0 },
+        { kReturn, 0 }
     };
     BytecodeExecutable exec;
     exec.chunks = {{chunk, 0}};
